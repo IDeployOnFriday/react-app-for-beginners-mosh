db189d9ff99fce8792d8889b44f07c31f9764076
ideployonfriday <ideployonfriday@gmail.com>
2025-09-02 15:05:49 +0100

    validation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,8 @@ import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
 
     const items = ['New York', 'San Francisco', 'Los Angeles', 'Chicago', 'Houston'];
+
+    if (items.length === 0) return <p>There are no items in the list</p>;
     
 
   return (
@@ -11,7 +13,7 @@ function ListGroup() {
       <h1>List</h1>
       <ul className="list-group">
         {items.map(item => (
-          <li>
+          <li key={item} className="list-group-item">
             {item}
           </li>
         ))}
