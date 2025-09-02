52595965b3ee18c247906a6397fd9c3fe40486f6
ideployonfriday <ideployonfriday@gmail.com>
2025-09-02 15:12:45 +0100

    conditional validation

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,15 +3,13 @@ import { Fragment } from "react/jsx-runtime";
 
 function ListGroup() {
 
-    let items = ['New York', 'San Francisco', 'Los Angeles', 'Chicago', 'Houston'];
-    items = [];
-
-    if (items.length === 0) return <p>There are no items in the list</p>;
-    
+    const items = ['New York', 'San Francisco', 'Los Angeles', 'Chicago', 'Houston'];
+    //items = [];
 
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 ? <p>There are no items in the list</p>: null}
       <ul className="list-group">
         {items.map(item => (
           <li key={item} className="list-group-item">
