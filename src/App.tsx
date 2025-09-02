4e2e47b5cff4d2bff22bb9415ed00875239d17e5
ideployonfriday <ideployonfriday@gmail.com>
2025-09-02 14:49:38 +0100

    react list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Message from "./message";
+import ListGroup from "./components/ListGroup";
 
 
 
 function App() {
-  return <div>
-    <Message />
-  </div>
+  return <div><ListGroup></ListGroup></div>
 }
 
 export default App
