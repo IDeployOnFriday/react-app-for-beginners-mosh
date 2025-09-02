913ef9b69078629144b194e40ed84f5407ae2fe2
ideployonfriday <ideployonfriday@gmail.com>
2025-09-02 15:25:53 +0100

    onclick console log

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,26 @@
 import { Fragment } from "react/jsx-runtime";
 
-
 function ListGroup() {
-
-    const items = ['New York', 'San Francisco', 'Los Angeles', 'Chicago', 'Houston'];
-    //items = [];
+  const items = [
+    "New York",
+    "San Francisco",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+  ];
+  //items = [];
 
   return (
     <>
       <h1>List</h1>
-      {items.length === 0 ? <p>There are no items in the list</p>: null}
+      {items.length === 0 ? <p>There are no items in the list</p> : null}
       <ul className="list-group">
-        {items.map(item => (
-          <li key={item} className="list-group-item">
+        {items.map((item) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={()=> console.log("click " ,item)}
+          >
             {item}
           </li>
         ))}
