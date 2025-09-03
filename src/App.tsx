a24109e6c650b1fad0b01ac698ce128c98c076b3
ideployonfriday <ideployonfriday@gmail.com>
2025-09-03 09:55:25 +0100

    button to open and close an alert window

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 
 
 function App() {
+
+  const [alertVisible, setAlertVisiblity] = useState(false);
+
   return <div>
-    <Alert>Hello <span>World</span></Alert>
-    <Button color="secondary" onClick={() => console.log("Button clicked" )}>Click Me</Button>
+
+    {alertVisible === true ? <Alert onClose={() => setAlertVisiblity(false)}  >My Alert</Alert> : null}
+    <Button color="secondary" onClick={() => setAlertVisiblity(true)}>Click Me</Button>
   </div>
 }
 
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -4,10 +4,15 @@ import type { ReactNode } from "react";
 interface Props {
   // allows passing html as children
   children: ReactNode;
+  onClose?: () => void;
 }
 
-function App({ children }: Props) {
-  return <div className="alert alert-primary">{children}</div>;
+function Alert({ children, onClose }: Props) {
+  return <div className="alert alert-primary alert-dismissible fade show" role="alert" >
+        {children}
+          <button type="button" className="btn-close" data-dismiss="alert" aria-label="Close" onClick={onClose}>
+          </button>
+    </div>;
 }
 
-export default App;
+export default Alert;
