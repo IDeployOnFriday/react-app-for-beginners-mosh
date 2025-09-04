e0ec708797a661b2155b36d60d3ccbb3afd976d1
ideployonfriday@gmail.com <ideployonfriday@gmail.com>
2025-09-04 09:47:16 +0100

    adding second button

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,14 @@ import Button from "./components/Button";
 function App() {
 
   const [alertVisible, setAlertVisiblity] = useState(false);
+    const [greetingVisiblity, setGreetingVisiblity] = useState(false);
 
   return <div>
 
     {alertVisible === true ? <Alert onClose={() => setAlertVisiblity(false)}  >My Alert</Alert> : null}
     <Button color="secondary" onClick={() => setAlertVisiblity(true)}>Click Me</Button>
+    <Button color="primary" onClick={() => setGreetingVisiblity(true)}>Show Me Message</Button>
+      {greetingVisiblity === true ? <p>My Message</p> : null}
   </div>
 }
 
